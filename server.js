0a5a05c8f0f94832bd3d68105a7735b14a76cdfc
Gaurav Garg <garg.gaurav.1989it@gmail.com>
2018-01-25 18:36:27 +0530

    added send file

--- server.js
+++ server.js
@@ -5,5 +5,12 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
+app.get('/[^\.]+$', function(req, res) {
+    res.set('Content-Type', 'text/html')
+        .sendFile("/dist/index.html", { "root": __dirname })
+})
+
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, () => {
+    console.log(`Node server listening on http://localhost:8080`);
+});
