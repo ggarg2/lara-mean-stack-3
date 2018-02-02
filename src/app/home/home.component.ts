047606f0f223472bd853f06c13200d52c7f38b24
Gaurav Garg <garg.gaurav.1989it@gmail.com>
2018-02-02 09:13:16 +0530

    added custom observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Subscription } from 'rxjs/Rx';
+import { Observer, Subscription } from 'rxjs/Rx';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -9,58 +9,69 @@ import { Observable } from 'rxjs';
 })
 export class HomeComponent implements OnInit, OnDestroy {
 
-  myFirstSubscription: Subscription;
-
   ngOnDestroy(): void {
     console.log("Home module noOnDestroy Called!")
-    this.myFirstSubscription.unsubscribe();
   }
 
   constructor() { }
 
   ngOnInit() {
 
-    let myFirstObservable : Observable<number> = Observable.interval(2000);
 
-    this.myFirstSubscription = myFirstObservable.subscribe(
-      (data: number) => {
-        console.log("Your normal data is ", data);
-      },
-      (error)=>{
-        console.log("Error is ", error)
-      },
-      ()=>{
-        console.log("Observable is done")
-      }
+    let myFirstObservable : Observable<string> = Observable.create( (observer: Observer<string>) => {
 
-    )
+      
+
+      setTimeout( ()=>{
+        observer.next("First Next is called")  
+      }, 2000)
+
+      setTimeout( ()=>{
+        observer.next("Second Next is called")  
+      }, 4000)
+
+      setTimeout( ()=>{
+        observer.complete()  
+      }, 4000)
+
+      setTimeout( ()=>{
+        observer.error("First error is called")  
+      }, 4000)
+
+      setTimeout( ()=>{
+        observer.next("Third Next is called")  
+      }, 6000)
+
+      setTimeout( ()=>{
+        observer.next("Fourth Next is called")  
+      }, 8000)
+
+
+      setTimeout( ()=>{
+        observer.error("Second error is called")  
+      }, 12000)
+
+      setTimeout( ()=>{
+        observer.complete()  
+      }, 5500)
+      
+
+    })
 
     myFirstObservable.subscribe(
-      (data: number) => {
-        console.log("Your normal data is ", data);
+      (data: string)=>{
+        console.log("Normal Data Handler ", data)
       },
-      (error)=>{
-        console.log("Error is ", error)
+
+      (error) =>{
+        console.log("Error handler is called ", error)
       },
-      ()=>{
-        console.log("Observable is done")
+
+      () => {
+        console.log("Process is completed")
       }
-
     )
-
-    myFirstObservable.subscribe(
-      (data: number) => {
-        console.log("Your normal data is ", data);
-      },
-      (error)=>{
-        console.log("Error is ", error)
-      },
-      ()=>{
-        console.log("Observable is done")
-      }
-
-    )
-
+    
 
   }
 
