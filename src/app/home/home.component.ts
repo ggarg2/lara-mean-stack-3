5ff1deb927a4d2924c00f18af38b383fc03a89e8
Gaurav Garg <garg.gaurav.1989it@gmail.com>
2018-02-02 08:51:07 +0530

    added interval observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
+import { Subscription } from 'rxjs/Rx';
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -6,13 +8,60 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit, OnDestroy {
+
+  myFirstSubscription: Subscription;
+
   ngOnDestroy(): void {
     console.log("Home module noOnDestroy Called!")
+    this.myFirstSubscription.unsubscribe();
   }
 
   constructor() { }
 
   ngOnInit() {
+
+    let myFirstObservable : Observable<number> = Observable.interval(2000);
+
+    this.myFirstSubscription = myFirstObservable.subscribe(
+      (data: number) => {
+        console.log("Your normal data is ", data);
+      },
+      (error)=>{
+        console.log("Error is ", error)
+      },
+      ()=>{
+        console.log("Observable is done")
+      }
+
+    )
+
+    myFirstObservable.subscribe(
+      (data: number) => {
+        console.log("Your normal data is ", data);
+      },
+      (error)=>{
+        console.log("Error is ", error)
+      },
+      ()=>{
+        console.log("Observable is done")
+      }
+
+    )
+
+    myFirstObservable.subscribe(
+      (data: number) => {
+        console.log("Your normal data is ", data);
+      },
+      (error)=>{
+        console.log("Error is ", error)
+      },
+      ()=>{
+        console.log("Observable is done")
+      }
+
+    )
+
+
   }
 
 }
