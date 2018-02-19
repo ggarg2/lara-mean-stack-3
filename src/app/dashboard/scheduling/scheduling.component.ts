ec3feb2e8ef6101352b3e9f37cd0a8d48f9b94ae
cruisecoders <icruisecoder007@outlook.com>
2018-02-19 09:36:45 +0530

    reactive module abstract framework

--- src/app/dashboard/scheduling/scheduling.component.ts
+++ src/app/dashboard/scheduling/scheduling.component.ts
@@ -1,4 +1,4 @@
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -9,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class SchedulingComponent implements OnInit {
 
   schedulingForm: FormGroup;
+
+  reservedNumbers: any[] = ["12345", "9876", "987654321", "1234567890"]
 
   // user:  any = {
   //   id: 1234,
@@ -40,12 +42,36 @@ export class SchedulingComponent implements OnInit {
         'username': new FormControl(user.userName,  Validators.required),
         'email' : new FormControl(user.emailId, [ Validators.required, Validators.email ]),
       }),
-      'phoneNumber' : new FormControl(user.phoneNumber, [ Validators.required ])
+      'hobbies': new FormArray([]),
+      'phoneNumber' : new FormControl(user.phoneNumber, [ Validators.required, this.validatePhoneNumber.bind(this) ])
     })
   }
 
   submitForm(){
     console.log(this.schedulingForm)
+  }
+
+  addHobbies(){
+    console.log("add hobbies called")
+    let inputFormControl = new FormControl("Test Hobbies", Validators.required);
+    (<FormArray>this.schedulingForm.get('hobbies')).push(inputFormControl)
+  }
+
+  validatePhoneNumber(control: FormControl) : {[s: string] : boolean}{
+    let value = control.value;
+    // if(value == 12345){
+    //   return { 'phoneNumberAvailability' : true};
+    // }
+
+    console.log(this.reservedNumbers)
+    console.log(value)
+    console.log("this.reservedNumbers.indexOf(value) "+this.reservedNumbers.indexOf(value))
+
+    if(this.reservedNumbers.indexOf(value) > -1){
+      return { 'phoneNumberAvailability' : true};
+    }
+
+    return null;
   }
 
 }
